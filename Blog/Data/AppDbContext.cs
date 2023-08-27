c5fe5b327a0d5b371a27570dcf63be65ceb1f8c3
Touseef90 <touseef.ghazanfar@gmail.com>
2023-08-27 11:24:01 +0500

    Lecture 07 done

--- Blog/Data/AppDbContext.cs
+++ Blog/Data/AppDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Blog.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Data
 {
-	public class AppDbContext : DbContext
+	public class AppDbContext : IdentityDbContext<IdentityUser>
 	{
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,5 +1,7 @@
 using Blog.Data;
 using Blog.Data.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,10 +11,66 @@ builder.Services.AddControllersWithViews();
 
 var provider = builder.Services.BuildServiceProvider();
 var configuration = provider.GetRequiredService<IConfiguration>();
+
+// For Entity Framework
 builder.Services.AddDbContext<AppDbContext>(op => op.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddTransient<IRepository, Repository>();
 
+// For Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(op =>
+{
+    op.Password.RequireNonAlphanumeric = false;
+	op.Password.RequireUppercase = false;
+    op.Password.RequireDigit = false;
+    op.Password.RequiredLength = 6;
+})
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+// Adding Authentication
+builder.Services.AddAuthentication(op =>
+{
+    op.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    op.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    op.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+});
+
 var app = builder.Build();
+
+try
+{
+	var scope = app.Services.CreateScope();
+
+	var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+	var usrMngr = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+	var roleMngr = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+	ctx.Database.EnsureCreated();
+
+	var adminRole = new IdentityRole("Admin");
+	if (!ctx.Roles.Any())
+	{
+		// create a role
+		roleMngr.CreateAsync(adminRole).GetAwaiter().GetResult();
+	}
+
+	if (!ctx.Users.Any(u => u.UserName == "admin"))
+	{
+		// create an admin
+		var adminUser = new IdentityUser
+		{
+			UserName = "admin",
+			Email = "admin@test.com"
+		};
+		var result = usrMngr.CreateAsync(adminUser, "password").GetAwaiter().GetResult();
+		// add role to user
+		usrMngr.AddToRoleAsync(adminUser, adminRole.Name).GetAwaiter().GetResult();
+	}
+}
+catch (Exception e)
+{
+	Console.WriteLine(e.Message);
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -24,9 +82,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
-
-//app.MapGet("/", () => "Hello World!");
 
 app.Run();
